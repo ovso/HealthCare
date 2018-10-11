241ba8eae0cadc3bfbb89e50da25721acb074ee7
jaeho <ovsoce@gmail.com>
2018-10-11 21:15:19 +0900

    Apply splash delay

--- app/src/main/java/io/github/ovso/healthcare/ui/splash/SplashActivity.java
+++ app/src/main/java/io/github/ovso/healthcare/ui/splash/SplashActivity.java
@@ -6,18 +6,31 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import io.github.ovso.healthcare.R;
 import io.github.ovso.healthcare.ui.main.MainActivity;
+import io.reactivex.Single;
+import io.reactivex.disposables.Disposable;
+import java.util.concurrent.TimeUnit;
 
 public class SplashActivity extends AppCompatActivity {
+
+  private Disposable subscribe;
 
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
 
-    navigateToMain();
+    subscribe =
+        Single.just("splash").delay(500, TimeUnit.MILLISECONDS).subscribe(o -> navigateToMain());
   }
 
   private void navigateToMain() {
     startActivity(new Intent(getApplicationContext(), MainActivity.class));
     finish();
+  }
+
+  @Override public void onBackPressed() {
+    super.onBackPressed();
+    if (subscribe != null) {
+      subscribe.dispose();
+    }
   }
 }
