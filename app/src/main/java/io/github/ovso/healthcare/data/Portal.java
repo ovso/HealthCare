131c4af13021dfa4f9cdb4928576edbcb2b33eaa
jaeho <ovsoce@gmail.com>
2018-11-13 23:37:30 +0900

    Change web url

--- app/src/main/java/io/github/ovso/healthcare/data/Portal.java
+++ app/src/main/java/io/github/ovso/healthcare/data/Portal.java
@@ -6,19 +6,22 @@ import lombok.Getter;
 
 @AllArgsConstructor @Getter public enum Portal {
   GOOGLE(R.id.action_google, "https://google.co.kr/search?q="),
-  NAVER(R.id.action_naver, "https://m.search.naver.com/search.naver?query="),
-  DAUM(R.id.action_daum, "https://m.search.daum.net/search?q=");
+  NAVER(R.id.action_naver, "https://m.search.naver.com/search.naver?where=m_video&query="),
+  DAUM(R.id.action_daum, "https://m.search.daum.net/search?w=vclip&q=");
 
   private int act_id;
   private String url;
 
   public static String toUrl(int act_id, String q) {
-    for (Portal portal : Portal.values()) {
-      if (act_id == portal.act_id) {
-        return portal.url + q;
-      }
+    switch (act_id) {
+      case R.id.action_google:
+        return Portal.GOOGLE.getUrl() + q + "&tbm=vid";
+      case R.id.action_naver:
+        return Portal.NAVER.getUrl() + q;
+      case R.id.action_daum:
+        return Portal.DAUM.getUrl() + q;
+      default:
+        return Portal.GOOGLE.getUrl() + q + "&tbm=vid";
     }
-
-    return Portal.GOOGLE.url + q;
   }
 }
