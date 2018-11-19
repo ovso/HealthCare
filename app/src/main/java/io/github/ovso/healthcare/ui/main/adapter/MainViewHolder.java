a8fd1ef31ac55d76f190424b9790f4e131654c0d
jaeho <ovsoce@gmail.com>
2018-11-20 01:29:41 +0900

    Remove old likebutton for MainViewHolder

--- app/src/main/java/io/github/ovso/healthcare/ui/main/adapter/MainViewHolder.java
+++ app/src/main/java/io/github/ovso/healthcare/ui/main/adapter/MainViewHolder.java
@@ -3,26 +3,22 @@ package io.github.ovso.healthcare.ui.main.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.OnClick;
 import com.airbnb.lottie.LottieAnimationView;
-import com.like.LikeButton;
-import com.like.OnLikeListener;
 import io.github.ovso.healthcare.R;
 import io.github.ovso.healthcare.data.db.model.DiseaseEntity;
 import io.github.ovso.healthcare.ui.base.adapter.BaseViewHolder;
 import io.github.ovso.healthcare.ui.base.listener.DiseaseOnItemClickListener;
 import lombok.Setter;
 
-public class MainViewHolder extends BaseViewHolder<DiseaseEntity> implements OnLikeListener {
+public class MainViewHolder extends BaseViewHolder<DiseaseEntity> {
 
   @BindView(R.id.code_text_view) TextView codeTextView;
   @BindView(R.id.disease_text_view) TextView diseaseTextView;
   @BindView(R.id.like_button) LottieAnimationView likeButton;
   @Setter private DiseaseOnItemClickListener<DiseaseEntity> onItemClickListener;
-  private boolean check;
 
   private MainViewHolder(View itemView) {
     super(itemView);
@@ -32,21 +28,20 @@ public class MainViewHolder extends BaseViewHolder<DiseaseEntity> implements OnL
     super.bind(entity);
     codeTextView.setText(entity.code);
     diseaseTextView.setText(entity.name);
-    likeButton.setOnClickListener(v -> {
+    likeButton.setTag(entity.like);
+  }
 
-      check = !check;
-      if (check) {
-        likeButton.setAnimation("like.json");
-        likeButton.playAnimation();
-      } else {
-        likeButton.cancelAnimation();
-        likeButton.clearAnimation();
-        likeButton.setProgress(0);
-      }
-    });
-    //likeButton.setOnLikeListener(this);
-    //likeButton.setLiked(entity.like);
+  @OnClick(R.id.like_button) void onLikeClick(View view) {
+    final boolean check = !((boolean) view.getTag());
+    if (check) {
+      likeButton.playAnimation();
+    } else {
+      likeButton.cancelAnimation();
+      likeButton.setProgress(0);
+    }
 
+    likeButton.setTag(check);
+    onItemClickListener.onItemLikeClick(data, check);
   }
 
   public static MainViewHolder create(ViewGroup parent) {
@@ -57,13 +52,5 @@ public class MainViewHolder extends BaseViewHolder<DiseaseEntity> implements OnL
 
   @OnClick(R.id.root_view) void onItemClick() {
     onItemClickListener.onItemClick(data);
-  }
-
-  @Override public void liked(LikeButton likeButton) {
-    onItemClickListener.onItemLikeClick(data, true);
-  }
-
-  @Override public void unLiked(LikeButton likeButton) {
-    onItemClickListener.onItemLikeClick(data, false);
   }
 }
